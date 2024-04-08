e04676d17aefd8cae5fe33fbd5e288d839030332
xxseoxx <ssss4482@naver.com>
2024-04-08 17:41:59 +0900

    99.js

---
+++ test0099.js
@@ -0,0 +1,12 @@
+var numbers = new Array();
+var numbers2 = new Array(4);
+
+console.log("numbers길이", numbers.length);
+console.log("numbers2길이", numbers2.length);
+
+
+var numbers3 = ["one", "two", "three"];
+console.log("numbers길이",  numbers3.length);
+
+var numbers4 = Array("one", "two", "three");
+console.log("numbers길이",  numbers4.length);
