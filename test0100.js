5532763c829177f603fb22bd287200f045165f28
xxseoxx <ssss4482@naver.com>
2024-04-08 17:42:09 +0900

    100.js

---
+++ test0100.js
@@ -0,0 +1,8 @@
+let ban1 = [90, 80, 85];
+let ban2 = [78, 88, 93];
+let total = ban1.concat(ban2);
+console.log(total);                         // 배열을 단순 합침: 괄호가 있음
+
+let phoneNum = ['010', '1234', '5678'];
+let phoneNumJoin = phoneNum.join("-");
+ console.log(phoneNumJoin);                 // 하나의 데이터: 괄호가 없음
