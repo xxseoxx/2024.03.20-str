79ac5da5143aa40f90daabbb064c6381dd3075cb
xxseoxx <ssss4482@naver.com>
2024-04-11 23:06:06 +0900

    123.js

---
+++ test0123.js
@@ -0,0 +1,23 @@
+// Timer관련(비동기)
+
+// Timer (동기, JS외 타 언어)
+// while(true){
+//     setGreenLight();
+//     sleep(30);
+//     setRedLight();
+//     sleep(60);
+// }
+
+// Timer관련(비동기)
+
+// function hello(){console.log("안녕!")}
+// setInterval(hello, 1000);
+
+// let count = 0;
+// let timer = setInterval(()=>{
+//     console.log("안녕!"); count++;
+//     if(count > 5) clearInterval(timer)
+// }, 1000);
+
+// setTimeout(()=>{console.log("안녕!");}, 3000);
+
